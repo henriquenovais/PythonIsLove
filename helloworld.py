c4c7e53b38c8df845817d680267511137bbfec18
Henrique Novais <henriquennoliveira@gmail.com>
2018-12-31 23:46:48 -0300

    Change file names and testing different printing formats in Python

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,10 @@
 #Python is a general purpose language
 #Python is object based and can run on multiple plattforms
-
 #Python is not pre installed on Windows!
 
+'''
+Multiline comments are done this way
+'''
+
 print('Hello world!')
-print('Test')
+
---
+++ printing.py
@@ -0,0 +1,6 @@
+#Testing print formats in python
+
+print('Test')
+print(' quote \' xD')
+print('double quote \" xP')
+print('')

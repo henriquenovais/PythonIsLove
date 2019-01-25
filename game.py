195d9f9a7a092208c77e627de7c76db471300c83
Henrique Novais <henriquennoliveira@gmail.com>
2019-01-24 23:20:14 -0300

    Implment line in pygame

--- game.py
+++ game.py
@@ -15,4 +15,45 @@ PI = 3.141592653
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 #Set caption to the game screen:
-pygame.display.set_caption("Professor Craven's Cool Game")
+pygame.display.set_caption("Henrique's Cool Game")
+
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+            done = True # Flag that we are done so we exit this loop
+ 
+    # --- Game logic should go here
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("User asked to quit.")
+        elif event.type == pygame.KEYDOWN:
+            print("User pressed a key.")
+        elif event.type == pygame.KEYUP:
+            print("User let go of a key.")
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            print("User pressed a mouse button")
+ 
+    # --- Drawing code should go here
+ 
+    # First, clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+    screen.fill(WHITE)
+    pygame.draw.line(screen, RED, [0, 0], [100, 100], 5)
+ 
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+
+pygame.quit()
+
+

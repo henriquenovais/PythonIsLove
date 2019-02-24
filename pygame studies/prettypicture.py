b86dc161c9359f4f088429e6625777b99e6d590c
Henrique Novais <henriquennoliveira@gmail.com>
2019-02-23 22:08:20 -0300

    Organize files, create sections based on python topics

---
+++ pygame studies/prettypicture.py
@@ -0,0 +1,61 @@
+# Import a library of functions called 'pygame'
+import pygame
+# Initialize the game engine
+pygame.init()
+
+# Define some colors:
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+BLUE     = (   0,   0, 255)
+PI = 3.141592653
+
+#Openning window and setting window size:
+size = (1300, 800)
+screen = pygame.display.set_mode(size)
+#Set caption to the game screen:
+pygame.display.set_caption("Henrique's Cool Game")
+
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+            done = True # Flag that we are done so we exit this loop
+ 
+    # --- Game logic should go here
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("User asked to quit.")
+        elif event.type == pygame.KEYDOWN:
+            print("User pressed a key.")
+        elif event.type == pygame.KEYUP:
+            print("User let go of a key.")
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            print("User pressed a mouse button")
+ 
+    # --- Drawing code should go here
+ 
+    # First, clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+    screen.fill(WHITE)
+    #Picture background
+
+
+
+ 
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+
+pygame.quit()
+

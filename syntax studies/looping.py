096aaf68aee99e2f3f3623061fba74ca4cd27f17
Henrique Novais <henriquennoliveira@gmail.com>
2019-03-07 05:29:05 -0300

    Implement loop problem 2

--- syntax studies/looping.py
+++ syntax studies/looping.py
@@ -1,5 +1,16 @@
 #This code is the solution of the looping problems in:
 # http://programarcadegames.com/index.php?lang=pt&chapter=back_to_looping
 
+#Solution 1
 for x in range(0,10):
     print("*", end=" ")
+
+#Solution 2
+for x in range(0,10):
+    print("*",end=" ")
+
+for x in range(0,5):
+    print("*",end=" ")
+
+for x in range(0,10):
+    print("*",end=" ")

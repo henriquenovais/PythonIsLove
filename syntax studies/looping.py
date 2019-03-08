af4f6e30784f620d676677abacc5f0ce9b1675e1
Henrique Novais <henriquennoliveira@gmail.com>
2019-03-08 06:56:03 -0300

    Insert problems through commenting and implement solution 3

--- syntax studies/looping.py
+++ syntax studies/looping.py
@@ -1,12 +1,18 @@
 #This code is the solution of the looping problems in:
 # http://programarcadegames.com/index.php?lang=pt&chapter=back_to_looping
-
+#Problem 1
+#Create loop that prints out the following text:
+# * * * * * * * * * *
 #Solution 1
 '''
 for x in range(0,10):
     print("*", end=" ")
 '''
-
+#Problem 2
+#Create loop that prints out the following text:
+# * * * * * * * * * *
+# * * * * *
+# * * * * * * * * * * * * * * * * * * * *
 #Solution 2
 '''
 for x in range(0,10):
@@ -18,3 +24,21 @@ for x in range(0,5):
 for x in range(0,10):
     print("*",end=" ")
 '''
+
+#Problem 3
+#Create loop that prints out the following text:
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+#Solution 3:
+for x in range(0,10):
+    print("")
+    for y in range(0,10):
+        print("*", end=" ")
